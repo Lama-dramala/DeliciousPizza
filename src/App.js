5c8000d63c4b63dcdbf7aa8fd6c6cbeaa31f6227
Lama-dramala <oleggord62@gmail.com>
2024-01-30 13:09:56 +0200

    Проверка:wq

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Routes, Route } from "react-router-dom";
-import axios from "axios";
 import { useDispatch } from "react-redux";
+import axios from "axios";
 
 import Header from "./components/Header";
 import Sorting from "./components/Sorting";
